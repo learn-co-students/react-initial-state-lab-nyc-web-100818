271786a04f9509ff99677d66d8c1ab3af953287d
Rexman17 <a.stone1794@gmail.com>
2018-12-10 20:05:26 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
 // your Bomb code here!
+import React from 'react';
+
+
+export default class Bomb extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+  render() {
+    // use a ternary to get the message AND THEN return it!
+    let bombMsg = this.state.secondsLeft === 0 ? <h1>Boom!</h1> : <h1>{this.state.secondsLeft} seconds left before I go boom!</h1>
+
+    // must return ONE element
+    return (
+      <div>{bombMsg}</div>
+    )
+  }
+
+
+}
